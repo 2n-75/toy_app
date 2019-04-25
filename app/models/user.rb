1314824c0888daba127318012172c3379f14fa45
2n-75 <may.2s.gp31@gmail.com>
2019-04-25 17:05:06 +0900

    ユーザーページの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@
 
 class User < ApplicationRecord
   has_many :microposts
-	validates :name, presence: true    # 「FILL_IN」をコードに置き換えてください
-  validates :email, presence: true    # 「FILL_IN」をコードに置き換えてください
+  validates :name, presence: true
+  validates :email, presence: true
 end
