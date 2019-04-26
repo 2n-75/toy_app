e300ac043eec64e4461e96041e5d3ba74ea61b5d
2n-75 <may.2s.gp31@gmail.com>
2019-04-26 13:09:35 +0900

    ユーザーページの追加

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  root 'users#index'
+  get  '/signup', to: 'users#new'
+  resources :microposts
+  resources :users
+
+  get  '/home', to: 'static_pages#home'
+  get  '/help', to: 'static_pages#help'
+  get  '/about', to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+end
