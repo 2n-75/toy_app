71ced2599989122976667fb89e6e4d2276ca68c2
2n-75 <may.2s.gp31@gmail.com>
2019-04-26 16:16:39 +0900

    loginの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
 
   def hello
     render html: 'hello, world!'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       redirect_to @user
     else
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+ end
 
   private
 
