bf872fd71e8c76ae7573b44d7cd76ceec650e1fe
2n-75 <may.2s.gp31@gmail.com>
2019-05-08 16:07:09 +0900

    homeに投稿フォームと投稿一覧表示を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'users/new'
-  root 'users#index'
   get  '/signup', to: 'users#new'
-  resources :microposts
-  resources :users
 
   get  '/home', to: 'static_pages#home'
   get  '/help', to: 'static_pages#help'
   get  '/about', to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: %i[new create edit update]
+  resources :microposts,          only: %i[create destroy]
 end
